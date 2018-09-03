1a4aede53e9c0b71fefb10dd8bf4d8a3f49ae5dc
luanmaxiansheng <1586740049@qq.com>
2018-09-03 20:39:21 +0800

    用户信息验证

--- src/main/java/com/example/mongodb/model/Account.java
+++ src/main/java/com/example/mongodb/model/Account.java
@@ -3,6 +3,8 @@ package com.example.mongodb.model;
 import lombok.Data;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.IndexDirection;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 /**
@@ -17,6 +19,7 @@ public class Account {
     private String password;
     private String sex;
     private String tel;
+    @Indexed(unique = true,direction = IndexDirection.DESCENDING)
     private int age;
     private String []hobby;
     private Organization organization;
